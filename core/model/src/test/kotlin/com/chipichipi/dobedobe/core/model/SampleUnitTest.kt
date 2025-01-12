3d10ff9de45edc266187cbfaa679ad4a0c8cee2f
murjune <sis9221302@gmail.com>
2025-01-13 04:56:18 +0900

    chore: add jvm plugin to :model

---
+++ core/model/src/test/kotlin/com/chipichipi/dobedobe/core/model/SampleUnitTest.kt
@@ -0,0 +1,24 @@
+package com.chipichipi.dobedobe.core.model
+
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDate
+import kotlin.test.Test
+
+class SampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        4 shouldBe 2 + 2
+    }
+
+    @Test
+    fun `compose_test`() = runTest {
+        delay(10)
+    }
+
+    @Test
+    fun `datetime_test`() {
+        LocalDate(2024, 1, 13).toString() shouldBe "2024-01-13"
+    }
+}
