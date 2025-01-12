7826b0b1d866b824764d228ba37adfa48a85cd65
murjune <sis9221302@gmail.com>
2025-01-12 23:35:07 +0900

    chore: update version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.junit5) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.protobuf) apply false
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.module.graph) apply false
 }
