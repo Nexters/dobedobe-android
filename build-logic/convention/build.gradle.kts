b3b096f5c3feb68316bb00673525deb12429fafd
murjune <sis9221302@gmail.com>
2025-01-13 01:26:28 +0900

    chore: build-logic base

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.chipichipi.dobedobe.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
