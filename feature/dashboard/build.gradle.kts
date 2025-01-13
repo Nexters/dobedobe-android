33f598c4ff3dc3af693426ca3ce1afe6b4961c23
murjune <sis9221302@gmail.com>
2025-01-13 20:33:03 +0900

    chore: apply feature plugin to :feature:dashBoard

--- feature/dashboard/build.gradle.kts
+++ feature/dashboard/build.gradle.kts
@@ -1,35 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dobedobe.feature)
 }
 
 android {
     namespace = "com.chipichipi.dobedobe.feature.dashboard"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
