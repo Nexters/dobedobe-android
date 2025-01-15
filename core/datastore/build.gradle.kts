3d6017daade6b939387b5300105aca93fd353e30
Junhyeok <njh042777@gmail.com>
2025-01-15 23:22:07 +0900

    protobuf datastore 세팅 (#7)
    * chore : add proguard rule for protobuf
    
    * chore : configure android.library convention plugin

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -4,9 +4,14 @@ plugins {
 
 android {
     namespace = "com.chipichipi.dobedobe.core.datastore"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
 }
 
 dependencies {
+    api(libs.androidx.dataStore)
     api(projects.core.datastoreProto)
     api(projects.core.model)
 
