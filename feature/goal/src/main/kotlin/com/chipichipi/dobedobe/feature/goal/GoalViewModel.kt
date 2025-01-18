9275095dc0bb5dd36e22c39011fa78b32b1558f7
murjune <sis9221302@gmail.com>
2025-01-18 12:08:03 +0900

    chore: add koin module to goal module

---
+++ feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/GoalViewModel.kt
@@ -0,0 +1,6 @@
+package com.chipichipi.dobedobe.feature.goal
+
+import androidx.lifecycle.ViewModel
+
+class GoalViewModel : ViewModel() {
+}
---
+++ feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/di/GoalModule.kt
@@ -0,0 +1,9 @@
+package com.chipichipi.dobedobe.feature.goal.di
+
+import com.chipichipi.dobedobe.feature.goal.GoalViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val goalBoardModule = module {
+    viewModelOf(::GoalViewModel)
+}
