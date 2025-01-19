e17a53b9707b3d15591e3a44700703bcac0f7967
Junhyeok <njh042777@gmail.com>
2025-01-19 21:27:32 +0900

    feat : app module ui base

--- app/src/main/kotlin/com/chipichipi/dobedobe/MainActivity.kt
+++ app/src/main/kotlin/com/chipichipi/dobedobe/MainActivity.kt
@@ -4,49 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
+import com.chipichipi.dobedobe.ui.DobeDobeApp
+import com.chipichipi.dobedobe.ui.rememberDobeDobeAppState
 import org.koin.androidx.compose.KoinAndroidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
+            val appState = rememberDobeDobeAppState()
+
             DobeDobeTheme {
                 KoinAndroidContext {
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                    ) { innerPadding ->
-                        Greeting(
-                            "Android",
-                            modifier = Modifier.padding(innerPadding),
-                        )
-                    }
+                    DobeDobeApp(
+                        appState = appState,
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DobeDobeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/kotlin/com/chipichipi/dobedobe/ui/DobeDobeAppState.kt
@@ -0,0 +1,31 @@
+package com.chipichipi.dobedobe.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+internal fun rememberDobeDobeAppState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController(),
+): DobeDobeAppState {
+    return remember(
+        coroutineScope,
+        navController,
+    ) {
+        DobeDobeAppState(
+            coroutineScope = coroutineScope,
+            navController = navController,
+        )
+    }
+}
+
+@Stable
+internal class DobeDobeAppState(
+    coroutineScope: CoroutineScope,
+    val navController: NavHostController,
+)
