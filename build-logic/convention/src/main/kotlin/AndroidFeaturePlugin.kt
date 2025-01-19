84610433e762330bed1b2b0c7d9bec5cdd7c0b0a
murjune <sis9221302@gmail.com>
2025-01-19 14:50:54 +0900

    chore: change delimeter - to .

--- build-logic/convention/src/main/kotlin/AndroidFeaturePlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeaturePlugin.kt
@@ -17,8 +17,8 @@ class AndroidFeaturePlugin : Plugin<Project> {
                 "implementation"(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
                 "implementation"(libs.findLibrary("androidx.navigation.compose").get())
                 "implementation"(platform(libs.findLibrary("koin.bom").get()))
-                "implementation"(libs.findLibrary("koin-android").get())
-                "implementation"(libs.findLibrary("koin-androidx-compose").get())
+                "implementation"(libs.findLibrary("koin.android").get())
+                "implementation"(libs.findLibrary("koin.androidx.compose").get())
                 "testImplementation"(libs.findLibrary("androidx.navigation.testing").get())
                 "androidTestImplementation"(
                     libs.findLibrary("androidx.lifecycle.runtimeTesting").get(),
