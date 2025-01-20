b556cef1f21ee66928a1dd4ea90b290382785e2e
Junhyeok <njh042777@gmail.com>
2025-01-20 23:07:20 +0900

    feat : Onboarding presentation base

--- app/src/main/kotlin/com/chipichipi/dobedobe/di/AppModule.kt
+++ app/src/main/kotlin/com/chipichipi/dobedobe/di/AppModule.kt
@@ -4,6 +4,7 @@ import com.chipichipi.dobedobe.core.data.di.dataModule
 import com.chipichipi.dobedobe.feature.dashboard.di.dashboardModule
 import com.chipichipi.dobedobe.feature.goal.di.goalModule
 import com.chipichipi.dobedobe.feature.setting.di.settingModule
+import com.chipichipi.dobedobe.onboarding.OnboardingViewModel
 import com.chipichipi.dobedobe.ui.MainViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
@@ -24,4 +25,5 @@ val appModule =
             featureModule,
         )
         viewModelOf(::MainViewModel)
+        viewModelOf(::OnboardingViewModel)
     }
---
+++ app/src/main/kotlin/com/chipichipi/dobedobe/onboarding/OnboardingScreen.kt
@@ -0,0 +1,93 @@
+package com.chipichipi.dobedobe.onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTextButton
+import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTextField
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+internal fun OnboardingRoute(
+    modifier: Modifier = Modifier,
+    viewModel: OnboardingViewModel = koinViewModel(),
+) {
+    OnboardingScreen(
+        modifier = modifier,
+        completeOnboarding = viewModel::completeOnboarding,
+    )
+}
+
+@Composable
+private fun OnboardingScreen(
+    completeOnboarding: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier =
+            modifier
+                .onboardingModifier(),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        val textState = rememberTextFieldState()
+
+        Text(
+            text = "어떤 목표를 이루고 싶나요?",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "목표 이름",
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            color = Color.Black.copy(0.6f),
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        DobeDobeTextField(
+            state = textState,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        // TODO : 클릭 시 Goal DB에 저장 필요
+        DobeDobeTextButton(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .heightIn(54.dp),
+            onClick = completeOnboarding,
+        ) {
+            Text(
+                "저장하기",
+            )
+        }
+    }
+}
+
+private fun Modifier.onboardingModifier() =
+    this.then(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp)
+            .padding(top = 120.dp, bottom = 42.dp)
+            .imePadding(),
+    )
