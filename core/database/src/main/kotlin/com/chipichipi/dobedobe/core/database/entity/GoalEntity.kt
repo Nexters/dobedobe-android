ab3cf4514db3584a9998e41c5dbb0547f25e9b59
murjune <sis9221302@gmail.com>
2025-01-23 01:17:14 +0900

    feat: add GoalEntity

---
+++ core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/entity/GoalEntity.kt
@@ -0,0 +1,21 @@
+package com.chipichipi.dobedobe.core.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.chipichipi.dobedobe.core.database.entity.GoalEntity.Companion.TABLE_NAME
+import com.chipichipi.dobedobe.core.model.Goal.State
+import kotlinx.datetime.Instant
+
+@Entity(tableName = TABLE_NAME)
+data class GoalEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val title: String,
+    val isPinned: Boolean,
+    val state: State,
+    val createdAt: Instant,
+    val completedAt: Instant?,
+) {
+    companion object {
+        const val TABLE_NAME = "Goal"
+    }
+}
