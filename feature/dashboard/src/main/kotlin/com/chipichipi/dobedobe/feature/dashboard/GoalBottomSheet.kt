9a3cb856f3319561d5605931dfd03a84a4ee96b4
murjune <sis9221302@gmail.com>
2025-01-22 16:36:16 +0900

    feat: GoalBottomSheet basic Ui

---
+++ feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/GoalBottomSheet.kt
@@ -0,0 +1,189 @@
+package com.chipichipi.dobedobe.feature.dashboard
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chipichipi.dobedobe.core.model.Goal
+import com.chipichipi.dobedobe.core.model.fakeGoals
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GoalBottomSheet(
+    goals: List<Goal>,
+    onGoalItemDone: (Goal) -> Unit,
+    onGoalItemClick: (Goal) -> Unit,
+) {
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            initialValue = SheetValue.PartiallyExpanded,
+            skipHiddenState = true,
+        ),
+    )
+
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+            GoalBottomSheetContent(
+                goals = goals,
+                onGoalItemDone = onGoalItemDone,
+                onGoalItemClick = onGoalItemClick,
+            )
+        },
+        sheetPeekHeight = 300.dp, // 임시 값
+    ) {}
+}
+
+@Composable
+private fun GoalBottomSheetContent(
+    goals: List<Goal>,
+    onGoalItemDone: (Goal) -> Unit,
+    onGoalItemClick: (Goal) -> Unit,
+) {
+    Column {
+        GoalBottomSheetHeader(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        GoalContent(
+            goals = goals,
+            onGoalItemDone = onGoalItemDone,
+            onGoalItemClick = onGoalItemClick,
+        )
+    }
+}
+
+@Composable
+private fun GoalBottomSheetHeader(
+    modifier: Modifier = Modifier,
+) {
+    // TODO: 검색 기능 추가, parameter 는 그때 추가
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            "나의 목표",  // TODO : stringResource 추가 필요
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+        IconButton(
+            modifier = Modifier
+                .size(42.dp)
+                .offset {
+                    IntOffset(x = 12.dp.roundToPx(), y = 0)
+                },
+            onClick = {},
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                modifier = Modifier.size(18.dp),
+                contentDescription = "목표 검색", // TODO : stringResource 추가 필요
+            )
+        }
+    }
+}
+
+@Composable
+private fun GoalContent(
+    goals: List<Goal>,
+    onGoalItemDone: (Goal) -> Unit,
+    onGoalItemClick: (Goal) -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeightIn(min = 200.dp), // TODO: 최소 높이 조절 필요
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        contentPadding = PaddingValues(horizontal = 24.dp),
+    ) {
+        items(goals) { goal ->
+            GoalItem(
+                goal = goal,
+                onGoalItemDone = onGoalItemDone,
+                onGoalItemClick = onGoalItemClick,
+            )
+        }
+    }
+}
+
+@Composable
+private fun GoalItem(
+    goal: Goal,
+    onGoalItemDone: (Goal) -> Unit,
+    onGoalItemClick: (Goal) -> Unit,
+) {
+    val isDoneGoal: Boolean = (goal.state == Goal.State.Done)
+    Surface(
+        color = Color.White,
+        shape = RoundedCornerShape(24.dp),
+        onClick = { onGoalItemClick(goal) },
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 17.dp, bottom = 18.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                modifier = Modifier.size(29.dp),
+                checked = isDoneGoal,
+                onCheckedChange = { onGoalItemDone(goal) },
+            )
+            Spacer(modifier = Modifier.width(11.dp))
+            Text(
+                text = goal.title,
+                fontWeight = FontWeight.Medium,
+                fontSize = 15.sp,
+                lineHeight = 21.sp,
+                style = MaterialTheme.typography.bodyLarge, // TODO : font 조절 필요 ,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun GoalBottomSheetPreview() {
+    GoalBottomSheet(
+        goals = fakeGoals(10),
+        onGoalItemDone = {},
+        onGoalItemClick = {},
+    )
+}
