3331b2f3e2b16d358a440c47db1cd22a1fe4ebce
murjune <sis9221302@gmail.com>
2025-01-23 01:18:00 +0900

    feat: load to koinModule

--- core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/di/DaoModule.kt
+++ core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/di/DaoModule.kt
@@ -1,8 +1,12 @@
 package com.chipichipi.dobedobe.core.database.di
 
+import com.chipichipi.dobedobe.core.database.dao.GoalDao
+import com.chipichipi.dobedobe.core.database.db.DobeDobeDatabase
 import org.koin.dsl.module
 
 val daoModule =
     module {
         includes(databaseModule)
+
+        single<GoalDao> { get<DobeDobeDatabase>().goalDao() }
     }
--- core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/di/DatabaseModule.kt
+++ core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/di/DatabaseModule.kt
@@ -1,7 +1,18 @@
 package com.chipichipi.dobedobe.core.database.di
 
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.chipichipi.dobedobe.core.database.db.DobeDobeDatabase
 import org.koin.dsl.module
 
 internal val databaseModule =
     module {
+        single<RoomDatabase> {
+            Room.databaseBuilder(
+                get(),
+                DobeDobeDatabase::class.java,
+                DobeDobeDatabase.DATABASE_NAME,
+            ).fallbackToDestructiveMigration() // TODO: 최초 배포 시 삭제
+                .build()
+        }
     }
