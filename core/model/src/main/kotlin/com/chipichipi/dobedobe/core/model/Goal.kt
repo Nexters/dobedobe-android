e0aa4649d1cca7b293cea29c832de52e6f387fae
murjune <sis9221302@gmail.com>
2025-01-22 23:34:13 +0900

    refactor: fakeGoals migrate to previewProvider

--- core/model/src/main/kotlin/com/chipichipi/dobedobe/core/model/Goal.kt
+++ core/model/src/main/kotlin/com/chipichipi/dobedobe/core/model/Goal.kt
@@ -14,20 +14,3 @@ data class Goal(
         Done,
     }
 }
-
-private val fakeGoals: List<Goal> =
-    listOf(
-        Goal(1, "3대 500 달성하기", true, Goal.State.Todo),
-        Goal(2, "영어 단어 하루 10개 외우기", false, Goal.State.Done),
-        Goal(3, "두비두비 출시", false, Goal.State.Todo),
-        Goal(4, "주간 러닝 3회", true, Goal.State.Todo),
-        Goal(5, "하루 2L 물 마시기", false, Goal.State.Todo),
-        Goal(6, "책 5권 읽기", false, Goal.State.Todo),
-        Goal(7, "일본 여행 다녀오기", false, Goal.State.Done),
-        Goal(8, "운동 루틴 정착", true, Goal.State.Done),
-    )
-
-fun fakeGoals(size: Int): List<Goal> =
-    List(size) {
-        fakeGoals[it % fakeGoals.size].copy(id = it.toLong())
-    }
--- feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/preview/GoalPreviewParameterProvider.kt
+++ feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/preview/GoalPreviewParameterProvider.kt
@@ -2,7 +2,6 @@ package com.chipichipi.dobedobe.feature.dashboard.preview
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.chipichipi.dobedobe.core.model.Goal
-import com.chipichipi.dobedobe.core.model.fakeGoals
 
 internal class GoalPreviewParameterProvider : PreviewParameterProvider<List<Goal>> {
     override val values: Sequence<List<Goal>>
@@ -14,4 +13,23 @@ internal class GoalPreviewParameterProvider : PreviewParameterProvider<List<Goal
                 fakeGoals(1),
                 emptyList(),
             )
+
+    companion object {
+        private val fakeGoals: List<Goal> =
+            listOf(
+                Goal(1, "3대 500 달성하기", true, Goal.State.Todo),
+                Goal(2, "영어 단어 하루 10개 외우기", false, Goal.State.Done),
+                Goal(3, "두비두비 출시", false, Goal.State.Todo),
+                Goal(4, "주간 러닝 3회", true, Goal.State.Todo),
+                Goal(5, "하루 2L 물 마시기", false, Goal.State.Todo),
+                Goal(6, "책 5권 읽기", false, Goal.State.Todo),
+                Goal(7, "일본 여행 다녀오기", false, Goal.State.Done),
+                Goal(8, "운동 루틴 정착", true, Goal.State.Done),
+            )
+
+        fun fakeGoals(size: Int): List<Goal> =
+            List(size) {
+                fakeGoals[it % fakeGoals.size].copy(id = it.toLong())
+            }
+    }
 }
