c76ee47f338dd76d8c1a082425a2c00af56cefde
Junhyeok <njh042777@gmail.com>
2025-01-23 21:20:23 +0900

    feat: Set DobeDobe TopAppBar height to 60

--- core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/TopAppBar.kt
@@ -36,6 +36,7 @@ fun DobeDobeTopAppBar(
         modifier = modifier,
         title = title,
         navigationIcon = navigationIcon,
+        expandedHeight = 60.dp,
         actions = actions,
         windowInsets = windowInsets,
         colors = colors,
