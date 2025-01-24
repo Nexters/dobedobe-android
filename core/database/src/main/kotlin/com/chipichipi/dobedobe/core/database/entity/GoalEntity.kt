ea4297213174c4295e33c5d6281a35a56e56a276
murjune <sis9221302@gmail.com>
2025-01-25 00:28:52 +0900

    chore: import Goal.State

--- core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/entity/GoalEntity.kt
+++ core/database/src/main/kotlin/com/chipichipi/dobedobe/core/database/entity/GoalEntity.kt
@@ -3,15 +3,16 @@ package com.chipichipi.dobedobe.core.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.chipichipi.dobedobe.core.database.entity.GoalEntity.Companion.TABLE_NAME
-import com.chipichipi.dobedobe.core.model.Goal.State
+import com.chipichipi.dobedobe.core.model.Goal
 import kotlinx.datetime.Instant
 
 @Entity(tableName = TABLE_NAME)
 data class GoalEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     val title: String,
     val isPinned: Boolean,
-    val state: State,
+    val state: Goal.State,
     val createdAt: Instant,
     val completedAt: Instant?,
 ) {
