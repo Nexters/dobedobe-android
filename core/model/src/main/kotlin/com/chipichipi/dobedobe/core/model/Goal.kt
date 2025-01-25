581294197dd59335f4aae3230b3fb8d3a109a746
murjune <sis9221302@gmail.com>
2025-01-25 14:38:23 +0900

    feat: goal todo factory function

--- core/model/src/main/kotlin/com/chipichipi/dobedobe/core/model/Goal.kt
+++ core/model/src/main/kotlin/com/chipichipi/dobedobe/core/model/Goal.kt
@@ -1,5 +1,6 @@
 package com.chipichipi.dobedobe.core.model
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 data class Goal(
@@ -10,11 +11,29 @@ data class Goal(
     val createdAt: Instant,
     val completedAt: Instant?,
 ) {
+    val isDone: Boolean
+        get() = (state == State.Done)
+
     init {
         if (isCompleted) {
             require(completedAt != null) { "completedAt should not be null when isCompleted is true" }
         } else {
             require(completedAt == null) { "completedAt should be null when isCompleted is false" }
+        }
+    }
+
+    companion object {
+        private val NO_ID = 0L
+        // TODO: 부생성자로 할지, 팩토리 메서드로 할지 고민중..
+        fun newTodo(title: String): Goal {
+            return Goal(
+                id = NO_ID,
+                title = title,
+                isPinned = false,
+                isCompleted = false,
+                createdAt = Clock.System.now(),
+                completedAt = null,
+            )
         }
     }
 }
