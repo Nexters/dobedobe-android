9bd89c7bcad5ad964f0e5cc62c9cf61fc0de21c8
Junhyeok <njh042777@gmail.com>
2025-01-26 19:11:02 +0900

    feat : add temporary wrapping structure for Switch

---
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/Switch.kt
@@ -0,0 +1,47 @@
+package com.chipichipi.dobedobe.core.designsystem.component
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
+
+/**
+ * TODO : Switch 컴포넌트 단순 Wrapper 임시 처리, 각 상태 디자인 정의 필요
+ */
+@Composable
+fun DobeDobeSwitch(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+) {
+    // TODO : 디자인시스템 나오면 컬러 정의 필요
+    Switch(
+        modifier = modifier,
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = Color.White,
+            checkedTrackColor = Color.Red,
+            uncheckedThumbColor = Color.Gray,
+            uncheckedTrackColor = Color.Transparent
+        )
+    )
+}
+
+@ThemePreviews
+@Composable
+private fun DobeDobeSwitchPreview() {
+    DobeDobeTheme {
+        DobeDobeSwitch(
+            checked = true,
+            onCheckedChange = {}
+        )
+    }
+}
