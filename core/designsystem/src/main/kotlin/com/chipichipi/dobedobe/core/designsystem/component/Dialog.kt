cfa2ffd2d7f452ad42813ebbfe1e02c6907f0604
Junhyeok <njh042777@gmail.com>
2025-01-26 18:34:36 +0900

    feat : add temporary wrapping structure for dialog

---
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/Dialog.kt
@@ -0,0 +1,81 @@
+package com.chipichipi.dobedobe.core.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
+
+@Composable
+fun DobeDobeDialog(
+    onDismissRequest: () -> Unit,
+    title: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
+    ) {
+        Surface(
+            modifier = modifier
+                .width(253.dp),
+            shape = RoundedCornerShape(16.dp),
+            color = Color.White
+        ) {
+            Column(
+                modifier = Modifier.padding(
+                    vertical = 24.dp,
+                    horizontal = 15.dp
+                ),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                content()
+            }
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun DobeDobeDialogPreview() {
+    DobeDobeTheme {
+        DobeDobeDialog(
+            onDismissRequest = {},
+            title = "TEST"
+        ) {
+            Button(
+                onClick = {}
+            ) {
+                Text(text = "TEST")
+            }
+        }
+    }
+}
