904b04ed0430e5761a0b29f52d42f9f4660e1d72
Junhyeok <njh042777@gmail.com>
2025-01-26 19:19:10 +0900

    feat : add openPlayStore Util

---
+++ feature/setting/src/main/kotlin/com/chipichipi/dobedobe/feature/setting/util/SettingUtil.kt
@@ -0,0 +1,15 @@
+package com.chipichipi.dobedobe.feature.setting.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+// TODO : 주소 확인 필요
+internal fun openPlayStore(context: Context) {
+    val intent = Intent(
+        Intent.ACTION_VIEW,
+        Uri.parse("https://play.google.com/store/apps/details?id=com.chipichipi.dobedobe")
+    )
+
+    context.startActivity(intent)
+}
