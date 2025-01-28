e7fa21b68a769bd3c2be0574292e4ede0e066ded
murjune <sis9221302@gmail.com>
2025-01-28 19:49:32 +0900

    feat: impl AddGoalViewModel

--- feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/AddGoalViewModel.kt
+++ feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/AddGoalViewModel.kt
@@ -1,5 +1,41 @@
 package com.chipichipi.dobedobe.feature.goal
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chipichipi.dobedobe.core.data.repository.GoalRepository
+import com.chipichipi.dobedobe.core.model.Goal
+import com.chipichipi.dobedobe.core.model.GoalTitleValidResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class GoalViewModel : ViewModel()
+class AddGoalViewModel(
+    private val goalRepository: GoalRepository,
+) : ViewModel() {
+
+    private val goalTitle: MutableStateFlow<String> = MutableStateFlow("")
+    val goalValidResult: StateFlow<GoalTitleValidResult> =
+        goalTitle
+            .map(Goal::validateTitle)
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = GoalTitleValidResult.Valid,
+            )
+
+    fun changeGoalTitle(title: String) {
+        goalTitle.value = title
+    }
+
+    fun addGoal() {
+        viewModelScope.launch {
+            if (goalValidResult.value.isValid()) {
+                goalRepository.addGoal(goalTitle.value)
+                return@launch
+            }
+        }
+    }
+}
