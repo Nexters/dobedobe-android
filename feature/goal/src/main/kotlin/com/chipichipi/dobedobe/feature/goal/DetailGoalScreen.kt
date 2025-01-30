31180d4fafeacc3361ad981b003671a16df4af55
murjune <sis9221302@gmail.com>
2025-01-30 19:53:15 +0900

    refactor: dobedobe Component

--- feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/DetailGoalScreen.kt
+++ feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/DetailGoalScreen.kt
@@ -10,15 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.CheckBox
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -26,14 +22,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
@@ -216,55 +207,16 @@ private fun GoalDeleteDialog(
     modifier: Modifier = Modifier,
 ) {
     if (!visible) return
+    // TODO : string resource 로 변경
     DobeDobeDialog(
-        modifier = modifier,
-        onDismissRequest = onDismiss,
-        // TODO : string resource 로 변경
         title = "목표를 삭제하시겠어요?",
-    ) {
-        Button(
-            onClick = onDismiss,
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors(
-                // TODO: color scheme 적용 - gray900
-                containerColor = Color(0xFF262C36),
-                // TODO: color scheme 적용 - white
-                contentColor = Color.White,
-            ),
-        ) {
-            Text(
-                // TODO: string resource 로 변경
-                text = "취소",
-                textAlign = TextAlign.Center,
-                // TODO: font 적용
-                style = TextStyle(fontSize = 17.sp, fontWeight = FontWeight.SemiBold),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 3.dp),
-            )
-        }
-        Button(
-            onClick = onConfirm,
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors(
-                // TODO: color scheme 적용 - white
-                containerColor = Color.White,
-                // TODO: color scheme 적용 - error
-                contentColor = Color(0xFFFF354D),
-            ),
-        ) {
-            Text(
-                // TODO: string resource 로 변경
-                text = "삭제",
-                // TODO: font 적용
-                style = TextStyle(fontSize = 17.sp, fontWeight = FontWeight.SemiBold),
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 3.dp),
-            )
-        }
-    }
+        primaryText = "취소",
+        secondaryText = "삭제",
+        onClickPrimary = onDismiss,
+        onClickSecondary = onConfirm,
+        onDismissRequest = onDismiss,
+        modifier = modifier,
+    )
 }
 
 @ThemePreviews
