03a711d4c3bc51279296e3e96888dca262d10b11
Junhyeok <njh042777@gmail.com>
2025-01-30 23:37:45 +0900

    아이콘 디자인시스템 리소스 정의 (#33)
    * chore : add icon res
    
    * feat : DobeDobeIcons

---
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/icon/DobeDobeIcons.kt
@@ -0,0 +1,17 @@
+package com.chipichipi.dobedobe.core.designsystem.icon
+
+import com.chipichpi.dobedobe.core.designsystem.R
+
+object DobeDobeIcons {
+    val Add = R.drawable.ic_add_14
+    val ArrowBack = R.drawable.ic_arrow_back_24
+    val Bookmark = R.drawable.ic_bookmark
+    val Cancel = R.drawable.ic_cancel_circle_16
+    val Checked = R.drawable.ic_checked_24
+    val Unchecked = R.drawable.ic_unchecked_24
+    val Edit = R.drawable.ic_edit_22
+    val EditPlaceholder = R.drawable.ic_edit_placeholder_52
+    val Search = R.drawable.ic_search_24
+    val EditMode = R.drawable.ic_edit_mode
+    val Setting = R.drawable.ic_setting_24
+}
