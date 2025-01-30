da58219cc0b6744b23050d60a5d80cf708d082ef
Junhyeok <njh042777@gmail.com>
2025-01-31 01:14:29 +0900

    feat : Apply setting design system resources

--- feature/setting/src/main/kotlin/com/chipichipi/dobedobe/feature/setting/component/SettingTopAppBar.kt
+++ feature/setting/src/main/kotlin/com/chipichipi/dobedobe/feature/setting/component/SettingTopAppBar.kt
@@ -1,16 +1,17 @@
 package com.chipichipi.dobedobe.feature.setting.component
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTopAppBar
 import com.chipichipi.dobedobe.core.designsystem.component.ThemePreviews
+import com.chipichipi.dobedobe.core.designsystem.icon.DobeDobeIcons
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,9 +28,9 @@ internal fun SettingTopAppBar(
                 onClick = navigateToBack,
             ) {
                 Icon(
-                    modifier = Modifier.size(24.dp),
-                    imageVector = Icons.Default.ArrowBackIosNew,
+                    painter = painterResource(DobeDobeIcons.ArrowBack),
                     contentDescription = null,
+                    tint = Color.Unspecified,
                 )
             }
         },
