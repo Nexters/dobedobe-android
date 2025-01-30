b9c5de1672df0390eb690345121cafc23611864f
Junhyeok <njh042777@gmail.com>
2025-01-30 22:20:28 +0900

    feat : apply colors and typography to Theme

--- core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/theme/Theme.kt
@@ -1,65 +1,41 @@
 package com.chipichipi.dobedobe.core.designsystem.theme
 
-import android.os.Build
-import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
-
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-    )
 
 // TODO : change Light/Dark color
 private val LightAndroidBackgroundTheme = BackgroundTheme(color = Color.White)
 private val DarkAndroidBackgroundTheme = BackgroundTheme(color = Color.White)
 
+object DobeDobeTheme {
+    val typography: DobeDobeTypography
+        @ReadOnlyComposable @Composable get() = LocalDobeDobeTypography.current
+
+    val colors: DobeDobeColors
+        @ReadOnlyComposable @Composable get() = LocalDobeDobeColors.current
+}
+
 @Composable
 fun DobeDobeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme =
-        when {
-            dynamicColor && supportsDynamicTheming() -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
-
     val backgroundTheme = if (darkTheme) DarkAndroidBackgroundTheme else LightAndroidBackgroundTheme
+    val typography = remember { DobeDobeTypography() }
+    val colors = remember { DobeDobeColors(darkTheme) }
 
     CompositionLocalProvider(
         LocalBackgroundTheme provides backgroundTheme,
+        LocalDobeDobeTypography provides typography,
+        LocalDobeDobeColors provides colors,
     ) {
         MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
             content = content,
         )
     }
 }
-
-@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
-private fun supportsDynamicTheming() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
