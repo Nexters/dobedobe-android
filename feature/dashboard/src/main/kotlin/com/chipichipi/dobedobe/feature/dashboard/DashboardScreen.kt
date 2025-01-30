0bd775d97c388b8ca77bf0cfd8f6af5a97cd407b
Junhyeok <njh042777@gmail.com>
2025-01-30 17:33:24 +0900

    chore : conflict

--- feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/DashboardScreen.kt
+++ feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/DashboardScreen.kt
@@ -8,10 +8,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,11 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -42,10 +34,6 @@ import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeBottomSheetSc
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeDialog
 import com.chipichipi.dobedobe.core.model.DashboardPhoto
 import com.chipichipi.dobedobe.feature.dashboard.component.DashboardEditMode
-import com.chipichipi.dobedobe.feature.dashboard.component.CollapsedPhotoFrame
-import com.chipichipi.dobedobe.feature.dashboard.component.DashboardBubble
-import com.chipichipi.dobedobe.feature.dashboard.component.DashboardCharacter
-import com.chipichipi.dobedobe.feature.dashboard.component.DashboardEditModeTopAppBar
 import com.chipichipi.dobedobe.feature.dashboard.component.DashboardTopAppBar
 import com.chipichipi.dobedobe.feature.dashboard.component.DashboardViewMode
 import com.chipichipi.dobedobe.feature.dashboard.component.ExpandedPhotoFrame
--- feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/component/DashboardViewMode.kt
+++ feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/component/DashboardViewMode.kt
@@ -2,13 +2,19 @@ package com.chipichipi.dobedobe.feature.dashboard.component
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.chipichipi.dobedobe.feature.dashboard.DashboardPhotoFramesState
 import com.chipichipi.dobedobe.feature.dashboard.model.DashboardPhotoState
@@ -18,7 +24,9 @@ import com.chipichipi.dobedobe.feature.dashboard.model.DashboardPhotoState
 internal fun SharedTransitionScope.DashboardViewMode(
     isViewMode: Boolean,
     photoState: List<DashboardPhotoState>,
+    bubbleTitle: String,
     photoFramesState: DashboardPhotoFramesState,
+    onChangeBubble: () -> Unit,
     onToggleExpansion: (Int) -> Unit,
     onToggleMode: () -> Unit,
     modifier: Modifier = Modifier,
@@ -27,15 +35,6 @@ internal fun SharedTransitionScope.DashboardViewMode(
         modifier = modifier,
         contentAlignment = Alignment.Center,
     ) {
-        if (isViewMode) {
-            DashboardCharacter(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 110.dp)
-                    .zIndex(0.5f),
-            )
-        }
-
         photoState.forEach { photo ->
             CollapsedPhotoFrame(
                 config = photo.config,
@@ -48,6 +47,27 @@ internal fun SharedTransitionScope.DashboardViewMode(
                     .fillMaxSize()
                     .zIndex(0f),
             )
+        }
+
+        if (isViewMode) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(Modifier.height(13.dp))
+                DashboardBubble(
+                    title = bubbleTitle,
+                    // TODO : font 적용
+                    textStyle = TextStyle(fontSize = 15.sp),
+                    // TODO: ColorScheme 적용
+                    modifier = Modifier.background(Color.White),
+                    onClick = onChangeBubble,
+                )
+                DashboardCharacter(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .zIndex(0.5f),
+                )
+            }
         }
     }
 }
