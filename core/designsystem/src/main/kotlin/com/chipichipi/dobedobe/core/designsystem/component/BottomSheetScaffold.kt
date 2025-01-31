553fc269bb4981ef464fdbeeacfa3fd6180783ff
Junhyeok <njh042777@gmail.com>
2025-02-01 00:15:07 +0900

    feat : add DobeDobeDragHandle

--- core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/BottomSheetScaffold.kt
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/BottomSheetScaffold.kt
@@ -3,16 +3,22 @@ package com.chipichipi.dobedobe.core.designsystem.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,7 +28,6 @@ fun DobeDobeBottomSheetScaffold(
     modifier: Modifier = Modifier,
     scaffoldState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState(),
     sheetPeekHeight: Dp = BottomSheetDefaults.SheetPeekHeight,
-    sheetDragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle() },
     topBar: @Composable (() -> Unit)? = null,
     snackbarHost: @Composable (SnackbarHostState) -> Unit = { SnackbarHost(it) },
     content: @Composable (PaddingValues) -> Unit,
@@ -34,12 +39,31 @@ fun DobeDobeBottomSheetScaffold(
             sheetContent = sheetContent,
             scaffoldState = scaffoldState,
             sheetPeekHeight = sheetPeekHeight,
-            sheetDragHandle = sheetDragHandle,
+            sheetDragHandle = {
+                DobeDobeDragHandle()
+            },
             topBar = topBar,
             snackbarHost = snackbarHost,
             content = content,
             sheetContainerColor = DobeDobeTheme.colors.white,
             containerColor = DobeDobeTheme.colors.white,
         )
+    }
+}
+
+@Composable
+private fun DobeDobeDragHandle(
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier
+            .padding(
+                top = 8.dp,
+                bottom = 12.dp,
+            ),
+        color = Color(0xFFD1D6DB),
+        shape = RoundedCornerShape(4.dp),
+    ) {
+        Box(Modifier.size(width = 48.dp, height = 4.dp))
     }
 }
