9c41cd1f920842b5c43c0a4734c467916203f9dd
murjune <sis9221302@gmail.com>
2025-01-31 23:43:08 +0900

    feat: add goal add logic

--- app/src/main/kotlin/com/chipichipi/dobedobe/onboarding/OnboardingScreen.kt
+++ app/src/main/kotlin/com/chipichipi/dobedobe/onboarding/OnboardingScreen.kt
@@ -12,16 +12,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.chipichipi.dobedobe.R
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeBackground
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTextButton
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTextField
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
+import com.chipichipi.dobedobe.feature.goal.errorMessage
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -29,22 +33,35 @@ internal fun OnboardingRoute(
     modifier: Modifier = Modifier,
     viewModel: OnboardingViewModel = koinViewModel(),
 ) {
+    val titleValidResult by viewModel.titleValidResult.collectAsStateWithLifecycle()
+    val errorMessage =
+        titleValidResult.errorMessage()
+            ?.let { stringResource(id = it) }
+
     OnboardingScreen(
+        errorMessage = errorMessage,
         modifier = modifier,
         completeOnboarding = viewModel::completeOnboarding,
+        onChangeTitle = viewModel::changeGoalTitle,
     )
 }
 
 @Composable
 private fun OnboardingScreen(
+    errorMessage: String?,
     completeOnboarding: () -> Unit,
+    onChangeTitle: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier.onboardingModifier(),
         horizontalAlignment = Alignment.Start,
     ) {
-        val textState = rememberTextFieldState()
+        val titleState = rememberTextFieldState()
+
+        LaunchedEffect(titleState.text) {
+            onChangeTitle(titleState.text.toString())
+        }
 
         Text(
             text = stringResource(R.string.onboarding_goal_prompt),
@@ -55,14 +72,14 @@ private fun OnboardingScreen(
         Spacer(modifier = Modifier.height(48.dp))
 
         DobeDobeTextField(
-            state = textState,
+            state = titleState,
             hint = stringResource(R.string.onboarding_goal_hint),
             supportMessage = stringResource(R.string.onboarding_goal_support_message),
+            errorMessage = errorMessage,
         )
 
         Spacer(modifier = Modifier.weight(1f))
 
-        // TODO : 클릭 시 Goal DB에 저장 필요
         DobeDobeTextButton(
             modifier = Modifier
                 .fillMaxWidth()
@@ -94,7 +111,10 @@ private fun OnboardingScreenPreview() {
     DobeDobeTheme {
         DobeDobeBackground {
             OnboardingScreen(
+                modifier = Modifier.fillMaxSize(),
+                errorMessage = "10글자 이상 입력해주세요",
                 completeOnboarding = {},
+                onChangeTitle = {},
             )
         }
     }
