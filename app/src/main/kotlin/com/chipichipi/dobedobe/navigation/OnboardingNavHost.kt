d1530acddc6f40441bfee5067f58067cc45291c6
Junhyeok <njh042777@gmail.com>
2025-02-02 21:03:35 +0900

    feat : Apply safe drawing to OnboardingNavHost

--- app/src/main/kotlin/com/chipichipi/dobedobe/navigation/OnboardingNavHost.kt
+++ app/src/main/kotlin/com/chipichipi/dobedobe/navigation/OnboardingNavHost.kt
@@ -1,6 +1,13 @@
 package com.chipichipi.dobedobe.navigation
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.chipichipi.dobedobe.onboarding.navigation.OnboardingAddGoalRoute
@@ -9,10 +16,19 @@ import com.chipichipi.dobedobe.onboarding.navigation.onboardingAddGoalScreen
 import com.chipichipi.dobedobe.onboarding.navigation.onboardingSelectCharacterScreen
 
 @Composable
-internal fun OnboardingNavHost() {
+internal fun OnboardingNavHost(
+    modifier: Modifier = Modifier
+) {
     val navController = rememberNavController()
 
     NavHost(
+        modifier = modifier
+            .fillMaxSize()
+            .windowInsetsPadding(
+                WindowInsets.safeDrawing.only(
+                    WindowInsetsSides.Vertical,
+                ),
+            ),
         navController = navController,
         startDestination = OnboardingAddGoalRoute
     ) {
