6ec77ac5ad5b987b72e3a831a86bd416fcf62a87
Junhyeok <njh042777@gmail.com>
2025-02-17 22:28:52 +0900

    feat : add GoalInfoCard design

--- feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/GoalBottomSheet.kt
+++ feature/dashboard/src/main/kotlin/com/chipichipi/dobedobe/feature/dashboard/GoalBottomSheet.kt
@@ -1,21 +1,27 @@
 package com.chipichipi.dobedobe.feature.dashboard
 
+import androidx.annotation.DrawableRes
 import androidx.compose.animation.core.snap
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,15 +34,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.chipichipi.dobedobe.core.designsystem.icon.DobeDobeIcons
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
+import com.chipichipi.dobedobe.core.designsystem.util.dropShadow
 import com.chipichipi.dobedobe.core.model.Goal
 import com.chipichipi.dobedobe.feature.dashboard.preview.GoalPreviewParameterProvider
 import com.chipichipi.dobedobe.feature.goal.component.GoalRow
@@ -161,6 +174,31 @@ private fun ColumnScope.GoalBottomSheetBody(
                 vertical = 16.dp,
             ),
         ) {
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    GoalInfoCard(
+                        modifier = Modifier.weight(1f),
+                        title = "올해의 목표", // TODO :
+                        count = goals.size,
+                        icon = DobeDobeIcons.Year2025,
+                        dropShadowColor = Color(0xFF6CD2FF)
+                    )
+                    GoalInfoCard(
+                        modifier = Modifier.weight(1f),
+                        title = "완료한 목표", // TODO :
+                        count = goals.filter { it.isCompleted }.size,
+                        icon = DobeDobeIcons.Checked,
+                        dropShadowColor = Color(0xFF58FF92)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
             if (isNotPartiallyExpanded) {
                 items(goals) { goal ->
                     GoalRow(
@@ -174,6 +212,68 @@ private fun ColumnScope.GoalBottomSheetBody(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun GoalInfoCard(
+    title: String,
+    @DrawableRes icon: Int,
+    dropShadowColor: Color,
+    count: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .border(
+                BorderStroke(
+                    width = 0.3.dp,
+                    color = DobeDobeTheme.colors.gray100
+                ),
+                shape = RoundedCornerShape(15.dp),
+            )
+            .clip(RoundedCornerShape(15.dp))
+            .background(DobeDobeTheme.colors.white)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        val heading1 = DobeDobeTheme.typography.heading1
+
+        Text(
+            text = buildAnnotatedString {
+                append(
+                    "$title\n"
+                )
+                withStyle(
+                    style = SpanStyle(
+                        fontStyle = heading1.fontStyle,
+                        fontWeight = heading1.fontWeight,
+                        fontSize = heading1.fontSize,
+                    )
+                ) {
+                    append("$count")
+                }
+            },
+            style = DobeDobeTheme.typography.body3,
+            color = DobeDobeTheme.colors.black
+        )
+
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = "year",
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .size(36.dp)
+                .align(alignment = Alignment.Bottom)
+                .dropShadow(
+                    shape = RoundedCornerShape(15.dp),
+                    color = dropShadowColor,
+                    blur = 40.dp,
+                    offsetX = 11.dp,
+                    offsetY = 17.dp,
+                ),
+        )
     }
 }
 
