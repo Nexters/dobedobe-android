1386d99107541ecc7fdf632ff8ba49fc18dd43ba
murjune <sis9221302@gmail.com>
2025-02-20 00:37:59 +0900

    feat: new design AddGoalScreen

--- feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/AddGoalScreen.kt
+++ feature/goal/src/main/kotlin/com/chipichipi/dobedobe/feature/goal/AddGoalScreen.kt
@@ -1,31 +1,29 @@
 package com.chipichipi.dobedobe.feature.goal
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeBackground
-import com.chipichipi.dobedobe.core.designsystem.component.DobeDobeTextButton
 import com.chipichipi.dobedobe.core.designsystem.theme.DobeDobeTheme
-import com.chipichipi.dobedobe.feature.goal.component.AddGoalTopAppBar
 import com.chipichipi.dobedobe.feature.goal.component.GoalEditor
+import com.chipichipi.dobedobe.feature.goal.component.GoalTopAppBar
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.compose.koinViewModel
@@ -39,9 +37,9 @@ fun AddGoalRoute(
 ) {
     val lifecycle = LocalLifecycleOwner.current.lifecycle
     val focusManager = LocalFocusManager.current
-    val goalValidResult by viewModel.goalValidResult.collectAsStateWithLifecycle()
+    val focusRequester = remember { FocusRequester() }
     val errorMessage =
-        goalValidResult.addGoalErrorMessage()
+        viewModel.goalValidResult.addGoalErrorMessage()
             ?.let { stringResource(id = it) }
 
     LaunchedEffect(Unit) {
@@ -54,19 +52,25 @@ fun AddGoalRoute(
             .launchIn(this)
     }
 
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     AddGoalScreen(
         modifier = Modifier
             .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTapGestures {
-                    focusManager.clearFocus()
-                }
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+            ) {
+                focusManager.clearFocus()
             }
             .imePadding(),
         errorMessage = errorMessage,
+        focusRequester = focusRequester,
         onShowSnackbar = onShowSnackbar,
         navigateToBack = navigateToBack,
-        onChangeGoalName = viewModel::changeGoalTitle,
+        titleState = viewModel.goalTitle,
         onAddGoal = viewModel::addGoal,
     )
 }
@@ -74,46 +78,35 @@ fun AddGoalRoute(
 @Composable
 private fun AddGoalScreen(
     errorMessage: String?,
+    titleState: TextFieldState,
+    focusRequester: FocusRequester,
     onShowSnackbar: suspend (String, String?) -> Boolean,
     navigateToBack: () -> Unit,
-    onChangeGoalName: (String) -> Unit,
     onAddGoal: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Scaffold(
         modifier = modifier,
         topBar = {
-            AddGoalTopAppBar(
+            GoalTopAppBar(
+                title = stringResource(R.string.feature_add_goal_top_bar_title),
                 navigateToBack = navigateToBack,
             )
         },
     ) { innerPadding ->
         GoalEditor(
             modifier = Modifier
-                .fillMaxSize()
                 .background(DobeDobeTheme.colors.white)
                 .padding(innerPadding)
                 .padding(horizontal = 24.dp)
-                .padding(top = 24.dp, bottom = 32.dp),
-            title = "",
-            header = stringResource(id = R.string.feature_detail_goal_todo_editor_header),
+                .padding(top = 8.dp, bottom = 12.dp),
+            titleState = titleState,
+            supportMessage = stringResource(id = R.string.feature_detail_goal_editor_support_message),
+            buttonText = stringResource(id = R.string.feature_add_goal_action_button),
             errorMessage = errorMessage,
-            onChangeTitle = onChangeGoalName,
+            focusRequester = focusRequester,
             onDone = onAddGoal,
-        ) {
-            DobeDobeTextButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(54.dp),
-                onClick = onAddGoal,
-            ) {
-                Text(
-                    text = stringResource(R.string.feature_add_goal_action_button),
-                    style = DobeDobeTheme.typography.heading2,
-                    color = DobeDobeTheme.colors.white,
-                )
-            }
-        }
+        )
     }
 }
 
@@ -122,13 +115,15 @@ private fun AddGoalScreen(
 private fun AddGoalScreenPreview() {
     DobeDobeTheme {
         DobeDobeBackground {
+            val textFieldState = rememberTextFieldState()
             AddGoalScreen(
                 modifier = Modifier
                     .fillMaxSize(),
+                titleState = textFieldState,
                 errorMessage = null,
+                focusRequester = FocusRequester(),
                 onShowSnackbar = { _, _ -> false },
                 navigateToBack = {},
-                onChangeGoalName = {},
                 onAddGoal = {},
             )
         }
