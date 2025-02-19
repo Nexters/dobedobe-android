3db63bbccc9cb92a6fa1a920ce26637d7d55bfd9
murjune <sis9221302@gmail.com>
2025-02-19 15:09:45 +0900

    feat: add CenterAlignedTopAppBar

--- core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/kotlin/com/chipichipi/dobedobe/core/designsystem/component/TopAppBar.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
@@ -42,6 +44,29 @@ fun DobeDobeTopAppBar(
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DobeDobeCenterAlignedTopAppBar(
+    modifier: Modifier = Modifier,
+    title: @Composable () -> Unit = {},
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(
+        containerColor = DobeDobeTheme.colors.gray50,
+    ),
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = title,
+        navigationIcon = navigationIcon,
+        expandedHeight = 48.dp,
+        actions = actions,
+        windowInsets = windowInsets,
+        colors = colors,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @ThemePreviews
 @Composable
 private fun DobeDobeTopAppBarPreview() {
@@ -52,6 +77,48 @@ private fun DobeDobeTopAppBarPreview() {
                     painter = painterResource(R.drawable.ic_arrow_back_24),
                     tint = Color.Unspecified,
                     contentDescription = null,
+                )
+            },
+            actions = {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_edit_24),
+                        tint = Color.Unspecified,
+                        contentDescription = null,
+                    )
+                    Icon(
+                        painter = painterResource(R.drawable.ic_setting_24),
+                        tint = Color.Unspecified,
+                        contentDescription = null,
+                    )
+                }
+            },
+        )
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+private fun DobeDobeCenterAlignedTopAppBarPreview() {
+    DobeDobeTheme {
+        DobeDobeCenterAlignedTopAppBar(
+            navigationIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_back_24),
+                    tint = Color.Unspecified,
+                    contentDescription = null,
+                )
+            },
+            title = {
+                Text(
+                    text = "Title",
+                    color = DobeDobeTheme.colors.gray700,
+                    style = DobeDobeTheme.typography.body1,
                 )
             },
             actions = {
